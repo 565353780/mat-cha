bbc8029f1c3b15f92f4f8033d0379145f9e6a3f3
chLi <565353780@qq.com>
2025-12-18 17:24:50 +0800

    add mkdir op

--- ln_models.sh
+++ ln_models.sh
@@ -1,6 +1,9 @@
 LOCAL_MODEL_PATH=/nvme0pnt/lichanghao/chLi/Model
 LOCAL_GIT_PATH=/nvme0pnt/lichanghao/github/RECON/MAtCha
 
+mkdir -p ${LOCAL_GIT_PATH}/Depth-Anything-V2/checkpoints/
+mkdir -p ${LOCAL_GIT_PATH}/mast3r/checkpoints/
+
 rm ${LOCAL_GIT_PATH}/Depth-Anything-V2/checkpoints/*
 rm ${LOCAL_GIT_PATH}/mast3r/checkpoints/*
 
--- train.sh
+++ train.sh
@@ -3,4 +3,6 @@ cd ../MAtCha
 python train.py \
   -s $HOME/chLi/Dataset/GS/haizei_1/input/ \
   -o $HOME/chLi/Dataset/GS/haizei_1/matcha/ \
-  --sfm_config unposed
+  --sfm_config unposed \
+  --depthanything_encoder vitg
+#--alignment_only
